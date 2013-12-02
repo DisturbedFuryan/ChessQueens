eeb8f393158ccf78e00e2a7e4e2ab7b1c805d57e
Marcin Rainka <marcin.rainka@gmail.com>
2013-12-02 23:04:29 +0100

    Method to check that the field is free.

--- Chessboard.cpp
+++ Chessboard.cpp
@@ -1,24 +1,82 @@
 #include <iostream>
 #include "Chessboard.hpp"
+#include "Vector.hpp"
 
 using namespace std;
 
-CChessboard::CChessboard(const unsigned n) : m_n(n) {
+CChessboard::CChessboard(const int n) : m_n(n) {
     m_fields = new bool*[n];
-    for (unsigned u = 0; u < n; ++u)
-        m_fields[u] = new bool[n];
+    for (int i = 0; i < n; ++i)
+        m_fields[i] = new bool[n];
+
+    for (int i = 0; i < m_n; ++i)
+        for (int j = 0; j < m_n; ++j)
+            m_fields[i][j] = false;
 }
 
 CChessboard::~CChessboard() {
-    for (unsigned u = 0; u < m_n; ++u)
-        delete [] m_fields[u];
+    for (int i = 0; i < m_n; ++i)
+        delete [] m_fields[i];
     delete [] m_fields;
 }
 
 void CChessboard::show() const {
-    for (unsigned u = 0; u < m_n; ++u) {
-        for (unsigned v = 0; v < m_n; ++v)
-            cout << m_fields[u][v] << ' ';
+    for (int i = 0; i < m_n; ++i) {
+        for (int j = 0; j < m_n; ++j)
+            cout << m_fields[i][j] << ' ';
         cout << endl;
     }
+}
+
+bool CChessboard::fieldIsFree(const CVector<int> pos) const {
+    // Check out the field and let's go...
+    if (m_fields[pos.y][pos.x]) return false;
+
+    // ...to the west.
+    for (int i = (pos.x - 1); i > -1; --i)
+        if (m_fields[pos.y][i]) return false;
+
+    // ...to the east.
+    for (int i = (pos.x + 1); i < m_n; ++i)
+        if (m_fields[pos.y][i]) return false;
+    
+    // ...to the north.
+    for (int i = (pos.y - 1); i > -1; --i)
+        if (m_fields[i][pos.x]) return false;
+
+    // ...to the south.
+    for (int i = (pos.y + 1); i < m_n; ++i)
+        if (m_fields[i][pos.x]) return false;
+
+    CVector<int> checkpoint;
+
+    // ...to the northwest.
+    checkpoint.set((pos.x - 1), (pos.y - 1));
+    while ((checkpoint.x > - 1) && (checkpoint.y > -1)) {
+        if (m_fields[checkpoint.y][checkpoint.x]) return false;
+        --checkpoint.x; --checkpoint.y;
+    }
+
+    // ...to the southwest.
+    checkpoint.set((pos.x + 1), (pos.y + 1));
+    while ((checkpoint.x < m_n) && (checkpoint.y < m_n)) {
+        if (m_fields[checkpoint.y][checkpoint.x]) return false;
+        ++checkpoint.x; ++checkpoint.y;
+    }
+
+    // ...to the northeast.
+    checkpoint.set((pos.x + 1), (pos.y - 1));
+    while ((checkpoint.x < m_n) && (checkpoint.y > -1)) {
+        if (m_fields[checkpoint.y][checkpoint.x]) return false;
+        ++checkpoint.x; --checkpoint.y;
+    }
+
+    // ...to the southeast.
+    checkpoint.set((pos.x - 1), (pos.y + 1));
+    while ((checkpoint.x > -1) && (checkpoint.y < m_n)) {
+        if (m_fields[checkpoint.y][checkpoint.x]) return false;
+        --checkpoint.x; ++checkpoint.y;
+    }
+
+    return true;
 }
--- Chessboard.hpp
+++ Chessboard.hpp
@@ -1,18 +1,22 @@
 #ifndef CHESSBOARD_HPP
 #define CHESSBOARD_HPP
 
+template<typename T> class CVector;
+
 //==================================================================================================
 class CChessboard {
 public:
-    CChessboard(const unsigned n);
+    CChessboard(const int n);
     ~CChessboard();
 
     void show() const;
 
 private:
-    const unsigned m_n;
+    const int m_n;
 
     bool** m_fields;
+
+    bool fieldIsFree(const CVector<int> pos) const;
 };
 //==================================================================================================
 
