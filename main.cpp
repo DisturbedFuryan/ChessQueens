aff81603c78eeb96c5af562eed6dd6f5905463c3
Marcin Rainka <marcin.rainka@gmail.com>
2013-12-04 11:55:39 +0100

    Creating the object of CChessboard class and using the putQueen() method.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
+#include <iostream>
 #include "Chessboard.hpp"
+
+using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char** argv) {
-
+    int n;
+    
+    // Get n from the user.
+    cout << "Type n: "; cin >> n; cout << endl;
+    
+    CChessboard chessboard(n);
+    
+    int counter = 1;
+    
+    chessboard.putQueen(0, counter);
+    
     return 0;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
